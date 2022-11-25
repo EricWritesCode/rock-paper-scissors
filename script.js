8788f0389249f256f06e9adef6150e708c20d8f1
Eric Vierling <118300434+EricWritesCode@users.noreply.github.com>
2022-11-25 13:41:49 -0600

    feat: add playRound function

--- script.js
+++ script.js
@@ -1,12 +1,60 @@
+// Randomly chooses a move for the computer
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3 + 1);
   switch (choice) {
     case 1:
-      return 'Rock';
+      return 'rock';
     case 2:
-      return 'Paper';
+      return 'paper';
     case 3:
-      return 'Scissors';
+      return 'scissors';
   }
 }
 
+
+//Plays a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+  let playerMove = playerSelection.toLowerCase();
+  // Including this in case computer choice is eventually exposed, easier than converting to caps afterwards
+  // let computerMove = computerSelection.toLowerCase();
+
+  if (playerMove === 'rock') {
+    switch (computerSelection) {
+      case 'rock':
+        return 'Tie!';
+      case 'paper':
+        return 'Computer wins! Paper beats rock.';
+      case 'scissors':
+        return 'You win! Rock beats scissors.';
+    }
+  } else if (playerMove === 'paper') {
+    switch (computerSelection) {
+      case 'rock':
+        return 'You win! Paper beats rock.';
+      case 'paper':
+        return 'Tie!';
+      case 'scissors':
+        return 'Computer wins! Scissors beats paper.';
+    }
+  } else if (playerMove === 'scissors') {
+    switch (computerSelection) {
+      case 'rock':
+        return 'Computer wins! Rock beats scissors.';
+      case 'paper':
+        return 'You win! Scissors beats paper.';
+      case 'scissors':
+        return 'Tie!';
+    }
+  } else {
+    return 'Invalid move!';
+  }
+
+}
+
+
+// Test Code
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(`Player: ${playerSelection}`);
+console.log(`Computer: ${computerSelection}`);
+console.log(playRound(playerSelection, computerSelection));
